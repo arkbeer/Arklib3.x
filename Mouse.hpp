35cea991398424195c601cc7652b71e022a05746
ArkBeer <ArkBeer@users.noreply.github.com>
2017-10-12 20:50:37 +0900

    Update Mouse.hpp

--- Mouse.hpp
+++ Mouse.hpp
@@ -1,17 +1,17 @@
 #pragma once
 #include<utility>
 namespace ark {
-	struct Mouse {
-		static auto GetScreenPosition() {
+	namespace Mouse {
+		auto GetScreenPosition() {
 			POINT pt;
 			GetCursorPos(&pt);
 			return std::make_pair(pt.x, pt.y);
 		}
-		static auto GetClientPosition(HWND hWnd) {
+		auto GetClientPosition(HWND hWnd) {
 			POINT pt;
 			GetCursorPos(&pt);
 			ScreenToClient(hWnd, &pt);
 			return std::make_pair(pt.x, pt.y);
 		}
-	};
+	}
 }
