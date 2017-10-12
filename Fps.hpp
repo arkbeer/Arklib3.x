78174788f62f629235e2120ae9353babfd95e91c
ArkBeer <ArkBeer@users.noreply.github.com>
2017-10-12 21:57:28 +0900

    Update Fps.hpp

--- Fps.hpp
+++ Fps.hpp
@@ -3,6 +3,7 @@
 #include<thread>
 namespace ark {
 	class Fps {
+		double fps;		
 		using timepoint = std::chrono::time_point < std::chrono::steady_clock >;
 		timepoint starttime;
 		const double passed() {
@@ -14,12 +15,13 @@ namespace ark {
 			return passed() - static_cast<double>(1 / fps);
 		}
 	public:
-		double fps;
+
 		Fps() :Fps(60.0) {}
 		Fps(double _fps) :fps(_fps), starttime(std::chrono::steady_clock::now()) {}
-        auto Now(){
-            return std::chrono::steady_clock::now();
-        }
+        	auto Now(){
+        	    return std::chrono::steady_clock::now();
+        	}
+		void SetFps(const double _fps){ fps=_fps; }
 		void Count() {
 			while (needed() < 0)std::this_thread::sleep_for(std::chrono::duration<int>(static_cast<int>(1 / fps)));
 			starttime = std::chrono::steady_clock::now();
